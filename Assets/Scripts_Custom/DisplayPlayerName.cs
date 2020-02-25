f631792075f442a24399c16668112e57e761a779
tiffhc <46658494+tiffhc@users.noreply.github.com>
2020-02-25 15:57:00 -0500

    Winner annoucement finished

---
+++ Assets/Scripts_Custom/DisplayPlayerName.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class DisplayPlayerName : MonoBehaviour
+{
+    public Text display_win; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        showWinner(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void showWinner()
+    {
+        if (Gameover.winner == "Player1")
+        {
+            display_win.text = "Player 1";
+        }
+        else if (Gameover.winner == "Player2")
+        {
+            display_win.text = "Player 2"; 
+        }
+    }
+}
