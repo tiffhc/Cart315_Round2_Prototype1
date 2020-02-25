28e22276d84102af91448e2fffb559838c278d9f
Liu WenYue <LiuWY0912@gmail.com>
2020-02-25 18:15:35 -0500

    TP1: WIP for burning the book

---
+++ Assets/Scripts_Custom/BurnBook.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurnBook : MonoBehaviour
+{
+    public GameObject book;
+    public GameObject MessagePanel;
+    public GameObject key;
+    
+    public float time = 5;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player1"))
+        {
+            if (gameObject.("fire") == null)
+                {
+                    Destroy(book);
+                    OpenMessagePanel("");
+                    key.SetActive(true);
+                }
+        }
+
+    }
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(time);
+        CloseMessagePanel();
+    }
+
+
+    public void OpenMessagePanel(string text)
+    {
+        MessagePanel.SetActive(true);
+    }
+
+    public void CloseMessagePanel()
+    {
+        MessagePanel.SetActive(false);
+    }
+}
