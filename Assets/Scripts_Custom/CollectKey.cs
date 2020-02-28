9e7b0ef5b7133668e6781e0c4c2a1329e249688e
Liu WenYue <LiuWY0912@gmail.com>
2020-02-28 01:47:36 -0500

    TP1: Added key collected script

---
+++ Assets/Scripts_Custom/CollectKey.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectKey : MonoBehaviour
+{
+    public GameObject key;
+    public GameObject MessagePanel;
+    public GameObject door;
+
+    public CollectMagnet CM;
+
+    public float time = 5;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player1"))
+        {
+            if (CM.magnetDestroyed == true)
+            {
+                Destroy(key);
+                OpenMessagePanel("");
+                key.SetActive(true);
+            }
+        }
+
+    }
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(time);
+        CloseMessagePanel();
+    }
+
+
+    public void OpenMessagePanel(string text)
+    {
+        MessagePanel.SetActive(true);
+    }
+
+    public void CloseMessagePanel()
+    {
+        MessagePanel.SetActive(false);
+    }
+}
