f95b5880230b022bd220222fba4560a21ce76a56
Liu WenYue <LiuWY0912@gmail.com>
2020-02-28 01:38:20 -0500

    TP1: Added collect magnet script

---
+++ Assets/Scripts_Custom/CollectMagnet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectMagnet : MonoBehaviour
+{
+    public GameObject magnet;
+    public GameObject MessagePanel;
+    public bool magnetDestroyed = false;
+
+    public float time = 5;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player1"))
+        {
+            Destroy(magnet);
+            magnetDestroyed = true;
+            OpenMessagePanel("");
+
+            Debug.Log("MAGNET destroyed!");
+        }
+    }
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(time);
+        CloseMessagePanel();
+    }
+
+
+    public void OpenMessagePanel(string text)
+    {
+        MessagePanel.SetActive(true);
+    }
+
+    public void CloseMessagePanel()
+    {
+        MessagePanel.SetActive(false);
+    }
+
+}
